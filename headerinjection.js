3f1181af0ca3cb0ba6734e11de5b54d3e6150e19
yaepearce <student.ep@youngsvilleacademy.org>
2026-04-27 12:59:58 -0400

    Create headerinjection.js

---
+++ headerinjection.js
@@ -0,0 +1,9 @@
+let elem = document.createElement("div");
+elem.innerHTML = "<link rel='stylesheet' href='https://yaepearce.github.io/web-dev/header.css'>"
+document.body.prepend(elem);
+
+fetch("https://yaepearce.github.io/web-dev/globalheader.html")
+.then((result) => result.text())
+.then((text) => {elem.innerHTML = elem.innerHTML + text;})
+.catch((e) => console.error(e)); 
+
