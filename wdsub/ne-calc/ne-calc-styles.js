76f68f2dd5fdf9c8d946b283fc931ddbf618e3ca
yaepearce <student.ep@youngsvilleacademy.org>
2026-05-29 13:33:38 -0400

    Update ne-calc-styles.js

--- wdsub/ne-calc/ne-calc-styles.js
+++ wdsub/ne-calc/ne-calc-styles.js
@@ -1,1 +1,104 @@
 
+//TODO: Replace p1NumStrats and p2NumStrats with query parameters
+let queryParams = new URLSearchParams(window.location.search);
+const P1_NUM_STRATS = queryParams.get("p1NumStrats");
+const P2_NUM_STRATS = queryParams.get("p2NumStrats");
+const PAYOFF_CONTENTS = "(<input type='number'>,<input type='number'>)";
+
+buildMatrix();
+
+
+function buildMatrix() {
+  let matrix = document.getElementById("matrix");
+  
+  //loop through (P1_NUM_STRATS + 1) times. Each iteration, make a row div
+  for (let i = 0; i < (P1_NUM_STRATS + 1); i++){
+    //create new row div
+    let newRow = document.createElement("div");
+    newRow.classList.add("matrix-row");
+    matrix.append(newRow);
+    
+    // loop through (P2_NUM_STRATS + 1) times. Each iteration, make a new cell
+    for(let j = 0; j < (P2_NUM_STRATS + 1); j++) {
+      //create a new cell
+      let newCell = document.createElement("div");
+      if(i == 0 && j == 0){
+        newCell.classList.add("empty-cell");
+      } else if(i == 0){
+        newCell.classList.add("strat-cell");
+        newCell.innerHTML = "t<sub>" + j + "</sub>";
+      } else if(j == 0){
+        newCell.classList.add("strat-cell");
+        newCell.innerHTML = "s<sub>" + i + "</sub>";
+      } else {
+        newCell.classList.add("payoff-cell") 
+        newCell.innerHTML = PAYOFF_CONTENTS
+      }
+      
+      newRow.append(newCell);
+      
+    }
+  }
+  
+}
+function randomize(){
+  let payoffArr = document.querySelectorAll(".payoff-cell input");
+  const MIN = -10;
+  const MAX =  20;
+  
+  for (const elem of payoffArr){
+    elem.value = Math.floor(Math.random() * (MAX + 1 - MIN) + MIN);
+    }
+  }
+
+function compute(){
+  let p1PayArr = document.querySelectorAll(".payoff-cell input:first-child");
+  let p2PayArr = document.querySelectorAll(".payoff-cell input:last-child");
+  let payCellArr = document.querySelectorAll(".payoff-cell")
+  
+  for (const elem of payCellArr){
+    if (elem.classList.contains("eliminated") == false)elem.classList.remove("eliminated");
+    if (elem.classList.contains("ne") == true)elem.classList.remove("ne");
+  }
+  
+  //loop through every column, finding p1's highest payoff out of the rows. 
+  for(let j = 0; j < P2_NUM_STRATS; j++){
+    let largest = -Infinity;
+    
+    //identify the highest payoff in this column
+   for (let i = 0; i < P1_NUM_STRATS; i++){
+      if(Number(p1PayArr[P2_NUM_STRATS*i + j].value) > Number(largest)) largest = p1PayArr[P2_NUM_STRATS*i + j].value
+    }
+    
+    //eliminate any cells which arent best responses
+    for (let i = 0; i < P1_NUM_STRATS; i++) {
+      if(Number(p1PayArr[P2_NUM_STRATS*i + j].value) != Number(largest)) payCellArr[P2_NUM_STRATS*i + j].classList.add("eliminated");
+    } 
+  }
+   
+  //loop through every row, finding p2's highest payoff out of the columns.
+  //loop through every column, finding p1's highest payoff out of the rows. 
+  for(let i = 0; i < P1_NUM_STRATS; i++){
+    let largest = -Infinity;
+    
+    //identify the highest payoff in this column
+   for (let j = 0; j < P2_NUM_STRATS; j++){
+      if(Number(p2PayArr[P2_NUM_STRATS*i + j].value) > Number(largest)) largest = p2PayArr[P2_NUM_STRATS*i + j].value
+    }
+    
+    //eliminate any cells which arent best responses
+    for (let j = 0; j < P2_NUM_STRATS; j++) {
+      if(Number(p2PayArr[P2_NUM_STRATS*i + j].value) != Number(largest)) payCellArr[P2_NUM_STRATS*i + j].classList.add("eliminated");
+    }  
+  }
+   
+  //give the ne class to any calls which are best responses for both players.
+  for(const elem of payCellArr){
+    if (elem.classList.contains("eliminated") == false)elem.classList.add("ne");
+  }
+  
+  
+}
+
+
+
